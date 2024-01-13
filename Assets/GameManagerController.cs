444e3372fd3e4e09d0dc2e22d98938d3c239d54e
igorbasko01 <igorbasko@gmail.com>
2024-01-13 18:31:49 +0200

    hit ball with accurate force and time

--- Assets/GameManagerController.cs
+++ Assets/GameManagerController.cs
@@ -7,6 +7,8 @@ public class GameManagerController : MonoBehaviour
     [SerializeField] private TargetMarkerController targetMarker;
     [SerializeField] private BallController ball;
     private IMoveable currentMoveable;
+    private float ballFlightSeconds = 0.5f;
+    private float gravity = -9.81f;
     
     // Start is called before the first frame update
     void Start()
@@ -45,7 +47,19 @@ public class GameManagerController : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.Return))
         {
-            ball.HandleBallHit(targetMarker.transform.position - ball.transform.position);
+            ball.HandleBallHit(CalculateForce());
         }
+    }
+
+    private Vector3 CalculateForce() {
+        var targetPosition = targetMarker.transform.position;
+        var ballPosition = ball.transform.position;
+        var distanceX = targetPosition.x - ballPosition.x;
+        var distanceZ = targetPosition.z - ballPosition.z;
+        var distanceY = targetPosition.y - ballPosition.y;
+        var velocityX = distanceX / ballFlightSeconds;
+        var velocityZ = distanceZ / ballFlightSeconds;
+        var velocityY = (distanceY - 0.5f * gravity * ballFlightSeconds * ballFlightSeconds) / ballFlightSeconds;
+        return new Vector3(velocityX, velocityY, velocityZ);
     }
 }
